af7fa5ede6dd73ca8a982a44453466dcaa8123b0
Akihiko SHIRAI <shirai@mail.com>
2016-05-31 08:44:47 +0900

    7-1:オブジェクトの関数への引き渡しと関数からの返し

--- Cpp07-1.cpp
+++ Cpp07-1.cpp
@@ -1,4 +1,9 @@
-﻿#include <iostream>
+﻿/*
+応用プログラミングA 第7週 クラスの詳細
+問題1：オブジェクトの関数への引き渡しと関数からの返し
+*/
+
+#include <iostream>
 #include <cmath> 
 using namespace std; 
 
@@ -17,28 +22,33 @@ public:
 
 double Triangle::area() {
 	double s;
-
-	s = (a + b + c) / 2;     return sqrt(s*(s - a)*(s - b)*(s - c));
-
+	s = (a + b + c) / 2;     
+	return sqrt(s*(s - a)*(s - b)*(s - c));
 }
 
-void Triangle::show() { cout << "辺a = " << a << "¥n";     cout << "辺b = " << b << "¥n";     cout << "辺c = " << c << "¥n";     cout << "面積= " << area() << "¥n"; }
+void Triangle::show() { 
+	cout << "辺a = " << a << "¥n";     
+	cout << "辺b = " << b << "¥n";     
+	cout << "辺c = " << c << "¥n";     
+	cout << "面積= " << area() << "¥n"; 
+}
 
 Triangle max_tri(Triangle ob1, Triangle ob2) {
-	if (ob1.area() > ob2.area())         return ob1;     else         return ob2;
-
+	if (ob1.area() > ob2.area())
+		return ob1;     
+	else         
+		return ob2;
 }
 
 int main() {
 	Triangle obj1, obj2, obj3;
 
-	obj1.set_a(3.0);     obj1.set_b(4.0);     obj1.set_c(5.0);     obj2.set_a(5.0);     obj2.set_b(12.0);     obj2.set_c(13.0);
-
+	obj1.set_a(3.0);     obj1.set_b(4.0);     obj1.set_c(5.0);     
+	obj2.set_a(5.0);     obj2.set_b(12.0);    obj2.set_c(13.0);
 	cout << "三角形¥n";     obj1.show();
-
 	cout << "三角形¥n";     obj2.show();
-
-	obj3 = max_tri(obj1, obj2);     cout << "大きい三角形¥n";     obj3.show();
-
+	obj3 = max_tri(obj1, obj2);     
+	cout << "大きい三角形¥n";     
+	obj3.show();
 	return 0;
 }
